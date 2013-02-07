b09e638dbfb0898eb721ae6bd18f666dcb7dc8c4
Vous <siddhaertha@gmail.com>
2013-02-06 20:02:46 -0500

    Updated Nation.py with Initial Functions
    Added the functions to get and set the name, began work on setting
    attributes.

--- pyns/Nation.py
+++ pyns/Nation.py
@@ -67,5 +67,62 @@ class Nation:
 		self.wcensus = 0
 		self.censusscore = 0
 		self.legislation = False ### To Do
-		self.happenings =  False### To Do
+		self.happenings =  False ### To Do
 		self.censusscoren = False ### To Do
+	
+	def getName(self):
+		return self.name
+
+	def setName(self, name):
+		self.name = name
+
+	def setAttribute(self, attribute, value):
+		if attribute == "name":
+			self.name = value
+		elif attribute == "fullname":
+			self.fullname = value
+		elif attribute == "type":
+			self.type = value
+		elif attribute == "motto":
+			self.motto = value
+		elif attribute == "category":
+			self.category = value
+
+		self.wa = False
+		self.endorsements = 0
+		self.gavote	= ""
+		self.scvote = ""
+		self.freedom = Freedom()
+		self.region = ""
+		self.population = 0
+		self.tax = 0
+		self.animal = ""
+		self.animaltrait = ""
+		self.currency = ""
+		self.flag = "" # A link
+		self.majorindustry = ""
+		self.crime = ""
+		self.sensibilities = ""
+		self.govtpriority = ""
+		self.govt = ""
+		self.govtdesc = ""
+		self.industrydesc = ""
+		self.notable = ""
+		self.admirable = ""
+		self.founded = 0
+		self.firstlogin = 0
+		self.lastlogin = 0
+		self.lastactivity = 0
+		self.influence = ""
+		self.freedomscores = FreedomScores()
+		self.publicsector = 0
+		self.deaths = Deaths()
+		self.leader = ""
+		self.capital = ""
+		self.religion = ""
+		self.customleader = ""
+		self.customcapital = ""
+		self.customreligion = ""
+		self.rcensus = 0
+		self.wcensus = 0
+		self.censusscore = 0
