90e9962ddf094588b052cd807b8f2d9cd4341cfb
Vous <siddhaertha@gmail.com>
2013-02-06 23:05:54 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='PyNS',
-    version='0.1.1',
+    version='0.1.2',
     author='Vous',
     author_email='maarja@outlook.com',
     packages=['pyns'],
